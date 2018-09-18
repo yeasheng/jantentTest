b7fed9ccb9d258cb61566319c8b1e2633e780f85
yansheng <yansheng@126.com>
2018-09-18 10:43:52 +0800

    ***

--- src/main/java/springboot/constant/WebConst.java
+++ src/main/java/springboot/constant/WebConst.java
@@ -22,12 +22,12 @@ public class WebConst {
     public static final String USER_IN_COOKIE = "S_L_ID";
 
     /**
-     * aes加密加盐
+     * aes加密加盐*****
      */
     public static String AES_SALT = "0123456789abcdef";
 
     /**
-     * 最大获取文章条数
+     * 最大获取文章条数*****
      */
     public static final int MAX_POSTS = 9999;
 
